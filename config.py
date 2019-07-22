03bb39928ce029ad722055e3ebc49c57ca7eae70
Artem Melnikov <melnikovam10@gmail.com>
2019-07-22 19:29:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ db_name="d47noii1g7t0fv"
 db_user="lxqvnmbwuoyshy"
 db_password="98355c75b76b94b0cda6ffa5f099d4f3b344ef5b421d531aa7f0455a6e5da41a"
 
-web_ip="localhost"
-port="5000"
+web_ip="195.201.111.238"
+port="8002"
 
 cols=5
